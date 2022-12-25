36d0188ad0344ecb0dd029fb2ae9ee61847c66bd
Nikita Zaitsev <zaitsev.nik2001@mail.ru>
2022-12-25 18:56:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите слово: ");
+
+string word = Console.ReadLine();
+
+char[] unequal = word.Distinct().ToArray();
+
+int length = unequal.Length;
+
+Console.WriteLine($"Количество неодинаковых букв: {length}");
